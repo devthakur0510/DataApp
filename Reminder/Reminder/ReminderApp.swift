9b588ecc83cccabaa3d63744190f4610df56a6cc
DeviOS <DeviOS>
2025-12-02 15:16:33 +0530

    add project

---
+++ Reminder/Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by Dev on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Reminder/ReminderTests/ReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderTests.swift
+//  ReminderTests
+//
+//  Created by Dev on 02/12/25.
+//
+
+import Testing
+@testable import Reminder
+
+struct ReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
