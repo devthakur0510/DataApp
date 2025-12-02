304b09b6dbec55c13fd195e875b83c11660d0979
DeviOS <DeviOS>
2025-12-02 17:27:29 +0530

    Add comment  in development

--- Reminder/ReminderTests/ContentViewTest.swift
+++ Reminder/ReminderTests/ContentViewTest.swift
@@ -4,32 +4,54 @@
 //
 //  Created by Dev on 02/12/25.
 //
-
 import XCTest
+import SwiftUI
+import UIKit
+@testable import Reminder
 
-final class ContentViewTest: XCTestCase {
+final class ContentViewTests: XCTestCase {
+    func test_containsHelloWorldLabel() {
+        let sut = ContentView()
+        let hosting = UIHostingController(rootView: sut)
+        hosting.loadViewIfNeeded()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let label = hosting.view.findLabel(withText: "Hello, world!")
+        XCTAssertNotNil(label, "ContentView should contain a label with text \"Hello, world!\"")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func test_containsImageView() {
+        let sut = ContentView()
+        let hosting = UIHostingController(rootView: sut)
+        hosting.loadViewIfNeeded()
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let imageViews = hosting.view.findImageViews()
+        XCTAssertFalse(imageViews.isEmpty, "ContentView should contain at least one image view (the system image)")
     }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+// MARK: - UIView helpers for searching subview hierarchy
+
+private extension UIView {
+    func findLabel(withText text: String) -> UILabel? {
+        if let label = self as? UILabel, label.text == text {
+            return label
         }
+        for sub in subviews {
+            if let found = sub.findLabel(withText: text) {
+                return found
+            }
+        }
+        return nil
     }
 
+    func findImageViews() -> [UIImageView] {
+        var results: [UIImageView] = []
+        if let iv = self as? UIImageView {
+            results.append(iv)
+        }
+        for sub in subviews {
+            results.append(contentsOf: sub.findImageViews())
+        }
+        return results
+    }
 }
